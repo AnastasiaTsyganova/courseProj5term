cbd36828882de0556621a605c99444c26e80c816
AnastasiaTsyganova <vika.lirik@gmail.com>
2022-11-07 18:43:51 +0300

    add phone

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,19 +1,15 @@
+import between.Phone;
 import java.io.*;
-import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        try (Socket socket = new Socket("127.0.0.1", 8000);
-             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream())))
+        try (Phone phone = new Phone("127.0.0.1", 8000))
         {
             System.out.println("Подключение с сервером установлено");
             String request = "Клиееееент";
-            writer.write(request);
-            writer.newLine();
-            writer.flush();
+            phone.writeLine(request);
 
-            String response =reader.readLine();
+            String response =phone.readLine();
             System.out.println(response);
         } catch (IOException e) {
             throw new RuntimeException(e);
