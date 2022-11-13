032cebfc978f10b36e947f066500dd7d0370ede1
AnastasiaTsyganova <vika.lirik@gmail.com>
2022-11-13 14:03:59 +0300

    add db

--- Server/src/Config.java
+++ Server/src/Config.java
@@ -1,3 +1,9 @@
 public class Config {
+    protected String dbHost="localhost";
+    protected String dbPort="3306";
+    protected String dbUser="root";
+    protected String dbPass="PolPin0609";
+    protected String dbName="courseProj5term ";
+
 
 }
